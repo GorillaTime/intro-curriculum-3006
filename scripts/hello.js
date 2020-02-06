e08b9b6324799b343a4b1d36b7b9c647ab1f9018
Gorilla <log1230gori@outlook.jp>
2020-02-06 17:18:33 +0900

    練習問題の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -5,5 +5,6 @@ module.exports = (robot) => {
 		const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
 		msg.send(lot +  ': ' + `<@${username}>`);
+		//
 	});
 };
